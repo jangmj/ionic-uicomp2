bdccfee851696cc39a9c939cc5c44746817eba6f
jangmj <jangmj@hanafos.com>
2018-04-17 21:06:41 +0900

    add navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,10 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items = [
+    {"id":1, name:"첫번째item"},
+    {"id":2, name:"두번째item"},
+  ];
+
+  disabledSwitch=false;
+
   userName:any;
 
   constructor(public navCtrl: NavController) {
-    this.userName = "아이오닉";
+    this.userName = "";
   }
 
+  //event handler
+  itemSelected(item) {
+    alert(item.id + " " + item.name);
+  }
+
+  //
+  myButton(event) {
+    if(typeof this.userName === 'undefined') {
+      alert('No name');
+      return false;
+    } else {
+      //alert(event.target.textContent + " " + event.clientX + " " + event.clientY);
+      this.navCtrl.push('bindPage',{name:this.userName});
+    }
+  }
 }
